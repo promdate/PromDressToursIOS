431601b4da0ff80930fa165322427ff36bc1b06a
Hamza <hkhan12@ocdsb.ca>
2019-03-21 11:01:44 -0400

    initial commit

---
+++ Prom Dress Tours/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Prom Dress Tours
+//
+//  Created by Hamza Khan on 2019-03-08.
+//  Copyright © 2019 Hamza Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func email(_ sender: UITextField) {
+    }
+    
+    @IBAction func password(_ sender: UITextField) {
+    }
+    
+    @IBAction func logIn(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+   
+
+}
+}
---
+++ Prom Dress ToursTests/Prom_Dress_ToursTests.swift
@@ -0,0 +1,34 @@
+//
+//  Prom_Dress_ToursTests.swift
+//  Prom Dress ToursTests
+//
+//  Created by Hamza Khan on 2019-03-08.
+//  Copyright © 2019 Hamza Khan. All rights reserved.
+//
+
+import XCTest
+@testable import Prom_Dress_Tours
+
+class Prom_Dress_ToursTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
